3a2684ced7cdff1a89ab23e2d7edaefdec625c4d
Spider Hand <creative.spider.hand@gmail.com>
2019-12-05 20:53:09 +0900

    fixed a bug

--- functions/index.js
+++ functions/index.js
@@ -3,18 +3,22 @@ const functions = require('firebase-functions')
 const admin = require('firebase-admin')
 admin.initializeApp()
 
-exports.deleteExpiredRooms = functions.pubsub.schedule('every monday 12:00').onRun((context) => {
+exports.deleteExpiredRooms = functions.pubsub.schedule('every wednesday 00:00').onRun((context) => {
   var db = admin.database()
 
   // Delete all rooms 1 day passed since it was created
-  db.once('value', (snapshot) => {
+  db.ref('/').once('value', (snapshot) => {
   	snapshot.forEach((childSnapshot) => {
-  		var createdAt = childSnapshot.child('createdAt').val()
-      var currentDate = Date.now()
-      var difference = currentDate - createdAt
+      if (childSnapshot.hasChild('createdAt')) {
+        var createdAt = childSnapshot.child('createdAt').val()
+        var currentDate = Date.now()
+        var difference = currentDate - createdAt
 
-      if (difference > 86400000) {
-        childSnapshot.remove()
+        if (difference > 86400000) {
+          childSnapshot.ref.remove()
+        }
+      } else {
+        childSnapshot.ref.remove()
       }
   	})
   })
