223b01004873a2c07a77c4aca5b3dfa5dd7d51d5
Spider Hand <creative.spider.hand@gmail.com>
2019-12-05 20:53:25 +0900

    turn on analytics

--- src/main.js
+++ src/main.js
@@ -30,7 +30,8 @@ var firebaseConfig = {
   projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.VUE_APP_FIREBASE_PROJECT_ID + ".appspot.com",
   messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.VUE_APP_FIREBASE_APP_ID
+  appId: process.env.VUE_APP_FIREBASE_APP_ID,
+  measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID
 }
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig)
