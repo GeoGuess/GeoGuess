3c37e54c2e74b5fa41a6b9f34c066677266b34c9
Bilel Jegham <BilelJegham@users.noreply.github.com>
2020-05-06 21:36:09 +0200

    Add value player

--- src/components/DialogRoom.vue
+++ src/components/DialogRoom.vue
@@ -109,6 +109,42 @@
           {
             text: '5',
             value: 5,
+          },
+          {
+            text: '6',
+            value: 6,
+          },   
+          {
+            text: '7',
+            value: 7,
+          },    
+          {
+            text: '8',
+            value: 8,
+          },         
+          {
+            text: '9',
+            value: 9,
+          },         
+          {
+            text: '10',
+            value: 10,
+          },             
+          {
+            text: '11',
+            value: 11,
+          },             
+          {
+            text: '12',
+            value: 12,
+          },              
+          {
+            text: '13',
+            value: 13,
+          },              
+          {
+            text: '14',
+            value: 14,
           },          
         ],
         timeLimitation: 0,
