fd19dc2bd7ce9f69f55be97300685bb469acc3cf
BilelJegham <bilel@jegham.com>
2020-05-09 00:32:49 +0200

    Env Var

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -21,7 +21,7 @@ if (process.env.NODE_ENV === 'production') {
     },
     updated () {
       console.log('New content is available; please refresh.')
-      // process.env.VUE_APP_NEED_UPDATE = true
+      process.env.VUE_APP_NEED_UPDATE = true
     },
     offline () {
       console.log('No internet connection found. App is running in offline mode.')
