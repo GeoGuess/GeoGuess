cf6d18e9504aa1fecf4d10e0f071b7b4e36b3fc1
BilelJegham <bilel@jegham.com>
2020-05-09 01:28:03 +0200

    50%

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -51,7 +51,7 @@
     </div>
     
     <v-container class="section" id="section-about"> 
-      <div style="width:100%;height:0;padding-bottom:100%;position:relative;"><iframe src="https://giphy.com/embed/d2jjuAZzDSVLZ5kI" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a href="https://giphy.com/gifs/lidl-voyages-confused-lost-d2jjuAZzDSVLZ5kI">via GIPHY</a></p>
+      <div style="width:50%;margin: 0 auto; height:0;padding-bottom:100%;position:relative;"><iframe src="https://giphy.com/embed/d2jjuAZzDSVLZ5kI" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div>
     </v-container>
     <v-container  class="section" id="section-limitation">
       <h3 class="section-header">LIMITATION</h3>
