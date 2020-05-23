2dde04b34628b5719d1fa208ba7693d368975fcc
Spider Hand <creative.spider.hand@gmail.com>
2020-05-23 22:41:17 +0900

    refactor (switch component dynamically instead of hardcoding in if statement)

---
+++ src/components/CardRoomPlayerName.vue
@@ -0,0 +1,60 @@
+<template>
+	<v-card color="#061422">
+		<v-card-title>
+			<span id="card-title">Type a player name.</span>
+		</v-card-title>
+		<v-card-text>
+			<v-container>
+				<v-row>
+          <v-col cols="12">
+              <v-text-field 
+                dark
+                maxlength="10"
+                v-model="playerName"></v-text-field>
+           </v-col>				
+				</v-row>
+			</v-container>
+		</v-card-text>
+    <v-card-actions>
+      <div class="flex-grow-1"></div>
+      <v-btn
+        dark
+        depressed
+        color="#FF5252"
+        @click="setPlayerName">NEXT</v-btn>
+      <v-btn
+        dark
+        depressed
+        color="#43B581"
+        @click="cancel">CANCEL</v-btn>
+    </v-card-actions>
+	</v-card>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+        playerName: '',			
+			}
+		},
+		methods: {
+			setPlayerName() {
+				// Pass room name to parent component
+				this.$emit('setPlayerName', this.playerName)
+			},
+			cancel() {
+				this.$emit('cancel')
+			}
+		}
+	}
+</script>
+
+<style scoped>
+  #card-title {
+    font-size: 16px;
+    font-weight: 500;
+    color: #FFFFFF;
+    opacity: 0.9;
+  }	
+</style>
--- src/components/DialogRoom.vue
+++ src/components/DialogRoom.vue
@@ -14,68 +14,14 @@
         With Friends
       </v-btn>
     </template>
-    <v-card color="#061422">
-      <v-card-title>
-        <span id="card-title">{{ cardTitle }}</span>
-      </v-card-title>
-      <v-card-text>
-        <v-container>
-          <v-row>
-            <v-col 
-              cols="12"
-              v-if="cardTitle == 'Type a room name'">
-              <v-text-field
-                dark
-                maxlength="10"
-                autofocus
-                :error-messages="errorMessage"
-                v-model="roomName"></v-text-field>
-            </v-col>
-            <v-col 
-              cols="4"
-              v-if="cardTitle == 'Set a room size'">
-              <v-select 
-                dark
-                v-model="roomSize"
-                :items="roomSizeItems"></v-select>
-            </v-col>
-            <v-col 
-              cols="6"
-              sm="4"
-              md="4"
-              lg="4"
-              xl="4"
-              v-if="cardTitle == 'Set a time limitation'">
-              <v-select
-                dark 
-                v-model="timeLimitation"
-                :items="timeLimitationItems"></v-select>
-            </v-col>
-            <v-col 
-              cols="12"
-              v-if="cardTitle == 'Type a player name'">
-              <v-text-field 
-                dark
-                maxlength="10"
-                v-model="playerName"></v-text-field>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-card-text>
-      <v-card-actions>
-        <div class="flex-grow-1"></div>
-        <v-btn
-          dark
-          depressed
-          color="#FF5252"
-          @click="clickNext">NEXT</v-btn>
-        <v-btn
-          dark
-          depressed
-          color="#43B581"
-          @click="cancel">CANCEL</v-btn>
-      </v-card-actions>
-    </v-card>
+    <component 
+      :is="currentComponent"
+      :errorMessage="errorMessage"
+      @searchRoom="searchRoom"
+      @setRoomSize="setRoomSize"
+      @setTimeLimitation="setTimeLimitation"
+      @setPlayerName="setPlayerName"
+      @cancel="cancel" />
   </v-dialog>
 </template>
 
@@ -83,102 +29,36 @@
   import firebase from 'firebase/app'
   import 'firebase/database'
 
+  import CardRoomName from '@/components/CardRoomName'
+  import CardRoomSize from '@/components/CardRoomSize'
+  import CardRoomTime from '@/components/CardRoomTime'
+  import CardRoomPlayerName from '@/components/CardRoomPlayerName'
+
   export default {
     data() {
       return {
         dialogRoom: false,
-        cardTitle: 'Type a room name',
         errorMessage: '',
         room: null,
-        playerNumber: null,
         roomName: '',
-        roomSize: 2,
-        roomSizeItems: [
-          {
-            text: '2',
-            value: 2,
-          },
-          {
-            text: '3',
-            value: 3,
-          },
-          {
-            text: '4',
-            value: 4,
-          },
-          {
-            text: '5',
-            value: 5,
-          },          
-        ],
-        timeLimitation: 0,
-        timeLimitationItems: [
-          {
-            text: 'Infinite',
-            value: 0,
-          },
-          {
-            text: '1',
-            value: 1,
-          },
-          {
-            text: '2',
-            value: 2,
-          },
-          {
-            text: '3',
-            value: 3,
-          },
-          {
-            text: '4',
-            value: 4,
-          },
-          {
-            text: '5',
-            value: 5,
-          }, 
-          {
-            text: '6',
-            value: 6,
-          },
-          {
-            text: '7',
-            value: 7,
-          },
-          {
-            text: '8',
-            value: 8,
-          },
-          {
-            text: '9',
-            value: 9,
-          },
-          {
-            text: '10',
-            value: 10,
-          },          
-        ],
-        playerName: '',
+        currentComponent: 'roomName',
       }
     },
+    components: {
+      'roomName': CardRoomName,
+      'roomSize': CardRoomSize,
+      'timeLimitation': CardRoomTime,
+      'playerName': CardRoomPlayerName,
+    },
     methods: {
-      clickNext() {
-        if (this.cardTitle == 'Type a room name') {
-          this.searchRoom()
-        }
-        else if (this.cardTitle == 'Set a room size') {
-          this.setRoomSize()
-        } else if (this.cardTitle == 'Set a time limitation') {
-          this.setTimeLimitation()
-        } else if (this.cardTitle == 'Type a player name') {
-          this.setPlayerName()
-        }
-      },
-      searchRoom() {
-        if (this.roomName == '') {
+      searchRoom(roomName) {
+        if (roomName == '') {
           this.errorMessage = 'Invalid name. Please try another.'
         } else {
-          this.room = firebase.database().ref(this.roomName)
+          this.room = firebase.database().ref(roomName)
+
+          // Save room name so it can be used in setPlayerName()
+          this.roomName = roomName
 
           this.room.once('value', (snapshot) => {
             var numberOfPlayers = snapshot.child('playerName').numChildren()
@@ -195,8 +75,7 @@
                   this.room.update({
                     createdAt: firebase.database.ServerValue.TIMESTAMP,
                   })
-                  // Ask the first player to decide the room size
-                  this.cardTitle = 'Set a room size'
+                  this.currentComponent = 'roomSize'
                 }
               })
 
@@ -205,15 +84,13 @@
               this.errorMessage = 'The first player is creating the room right now. Please wait and try again.'
 
             } else if (numberOfPlayers >= snapshot.child('size').val()) {
-              // In case the room is already full
               this.errorMessage = 'This room is already full. Please try another.'
 
             } else {
               // Put other player's tentative name
               this.room.child('playerName/player' + this.playerNumber).set('player' + this.playerNumber, (error) => {
                 if (!error) {
-                  // Let the other players decide the player name
-                  this.cardTitle = 'Type a player name'
+                  this.currentComponent = 'playerName'
                 }
               })
             }
@@ -221,27 +98,26 @@
           })
         }
       },
-      setRoomSize() {
+      setRoomSize(roomSize) {
         this.room.update({
-          size: this.roomSize
+          size: roomSize
         }, (error) => {
           if (!error) {
-            // Ask the first player to set time limitation
-            this.cardTitle = 'Set a time limitation'
+            this.currentComponent = 'timeLimitation'
           }
         })
       },
-      setTimeLimitation() {
+      setTimeLimitation(timeLimitation) {
         this.room.update({
-          timeLimitation: this.timeLimitation
+          timeLimitation: timeLimitation
         }, (error) => {
           if (!error) {
-            this.cardTitle = 'Type a player name'
+            this.currentComponent = 'playerName'
           }
         })
       },
-      setPlayerName() {
-        this.room.child('playerName/player' + this.playerNumber).set(this.playerName, (error) => {
+      setPlayerName(playerName) {
+        this.room.child('playerName/player' + this.playerNumber).set(playerName, (error) => {
           if (!error) {
             // Start the game
             this.$router.push({
@@ -256,7 +132,7 @@
       },
       cancel() {
         // Reset
-        this.cardTitle = 'Type a room name'
+        this.currentComponent = 'roomName'
         this.roomName = ''
         this.errorMessage = ''
 
@@ -285,12 +161,5 @@
     height: 44px;
     width: 240px;
     border-radius: 40px;
-  }
-
-  #card-title {
-    font-size: 16px;
-    font-weight: 500;
-    color: #FFFFFF;
-    opacity: 0.9;
   }
 </style>
