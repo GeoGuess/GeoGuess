2bf9ff781e8a365dc0d1dc002d905cd98bb26d2a
BilelJegham <bilel@jegham.com>
2020-05-31 00:25:24 +0200

    Refacto + add detail

--- src/components/StreetView.vue
+++ src/components/StreetView.vue
@@ -2,58 +2,106 @@
   <div id="game-page">
     <div id="street-view-container">
       
-        <HeaderGame 
-          :score="score"
-          :round="round" />
+      <HeaderGame
+        ref="header"
+        :score="scoreHeader"
+        :round="round"
+        :roomName="roomName"
+        :remainingTime="remainingTime" />
+        
       <div id="game-interface">
         <div id="street-view">
         </div>
+        
         <div id="game-interface--overlay">
-        <Maps
-          :randomLatLng="randomLatLng"
-          :round="round"
-          :score="score"
-          @resetLocation="resetLocation"
-          @calculateDistance="updateScore"
-          @goToNextRound="goToNextRound"
-          @playAgain="playAgain" />
+      <Maps
+        ref="map"
+        :randomLatLng="randomLatLng"
+        :roomName="roomName"
+        :playerNumber="playerNumber"
+        :isReady="isReady"
+        :round="round"
+        :score="score"
+        @resetLocation="resetLocation"
+        @calculateDistance="updateScore"
+        @showResult="showResult"
+        @goToNextRound="goToNextRound"
+        @finishGame="finishGame" />
         </div>
-      </div>
+        </div>
     </div>
     <v-overlay 
       :value="overlay"
       opacity="0.8" 
-      z-index="2"/>
-      
-    <v-alert type="warning" dismissible class="warning-alert" v-model="isVisibleDialog" tile>
+      z-index="1"/>
+    <DialogMessage 
+      :dialogMessage="dialogMessage"
+      :dialogTitle="dialogTitle"
+      :dialogText="dialogText" />
+    <v-alert type="warning" dismissible class="warning-alert" v-if="isVisibleDialog" tile>
      <b>Nearby Position</b> : Unfortunately, we were unable to find a random position in the defined location. However, we have found one nearby 😉
     </v-alert>
   </div>
 </template>
 
 <script>
+  import firebase from 'firebase/app'
+  import 'firebase/database'
+
   import HeaderGame from '@/components/HeaderGame'
   import Maps from '@/components/Maps'
-  
+  import DialogMessage from '@/components/DialogMessage'
+
   import randomPositionInPolygon from 'random-position-in-polygon'
   import axios from 'axios'
   import * as turfModel from '@turf/helpers'
   import booleanPointInPolygon from '@turf/boolean-point-in-polygon'
-
+  
   export default {
+    props:{
+      'roomName': {
+        default: null,
+        type: String
+      },
+      'playerNumber': {
+        default: null,
+        type: Number
+      },
+      'placeGeoJson': {
+        default: null,
+        type: Object
+      },
+      'multiplayer': {
+        default: false,
+        type: Boolean
+      }
+    },
     components: {
       HeaderGame,
       Maps,
+      DialogMessage,
     },
-    props: ["placeGeoJson"],
     data() {
       return {
         randomLatLng: null,
+        randomLat: null,
+        randomLng: null,
         score: 0,
+        scoreHeader: 0,
         round: 1,
+        timeLimitation: 0,
+        remainingTime: 0,
+        hasTimerStarted: false,
+        hasLocationSelected: false,
         overlay: false,
+        room: null,
+        isReady: false,
+        dialogMessage: this.multiplayer,
+        dialogTitle: 'Waiting for other players...',
+        dialogText: '',
         cptNotFoundLocation: 0,
         isVisibleDialog: false,
+        panorama: null,
       }
     },
     methods: {
@@ -65,7 +113,19 @@
           radius: 100000,
           source: 'outdoor',
         }, this.checkStreetView)
-        
+      },
+      loadDecidedStreetView() {
+        // Other players load the decided streetview the first player loaded
+        var service = new google.maps.StreetViewService()
+        service.getPanorama({
+          location: {
+            lat: this.randomLat,
+            lng: this.randomLng,
+          },
+          preference: 'nearest',
+          radius: 100000,
+          source: 'outdoor',
+        }, this.checkStreetView)        
       },
       getRandomLatLng() {
         if(this.placeGeoJson != null){
@@ -82,81 +142,225 @@
       checkStreetView(data, status) {
         // Generate random streetview until the valid one is generated
         if (status == 'OK') {
-          var panorama = new google.maps.StreetViewPanorama(document.getElementById('street-view'))
-          panorama.setOptions({
+          this.panorama.setOptions({
             addressControl: false,
             fullscreenControl: false,
             motionTracking: false,
             motionTrackingControl: false,
             showRoadLabels: false,
           })
-          panorama.setPano(data.location.pano)
-          panorama.setPov({
+          this.panorama.setPano(data.location.pano)
+          this.panorama.setPov({
             heading: 270,
             pitch: 0,
           })
-          
+                    
           if(this.placeGeoJson != null && this.cptNotFoundLocation < 3 && !booleanPointInPolygon(turfModel.point([data.location.latLng.lng(), data.location.latLng.lat()]), this.placeGeoJson)){
             this.loadStreetView()
-            this.cptNotFoundLocation++;
+            this.cptNotFoundLocation++; 
           }else{
+             // If 3 times Street View does not find location in the polygon placeGeo print warning message
             if(this.placeGeoJson != null && !booleanPointInPolygon(turfModel.point([data.location.latLng.lng(), data.location.latLng.lat()]), this.placeGeoJson)) {
               this.isVisibleDialog = true;
             }
             // Save the location's latitude and longitude
             this.randomLatLng = data.location.latLng
             this.cptNotFoundLocation = 0;
+
+            if(this.multiplayer){
+              // Put the streetview's location into firebase
+              this.room.child('streetView/round' + this.round).set({
+                latitude: this.randomLatLng.lat(),
+                longitude:this.randomLatLng.lng(),
+                warning: this.isVisibleDialog
+              })
+            }
           }
+
+
         } else {
           this.loadStreetView()
         }
       },
       resetLocation(){
-        var panorama = new google.maps.StreetViewPanorama(document.getElementById('street-view'))
-        panorama.setOptions({
+        this.panorama.setOptions({
             addressControl: false,
             fullscreenControl: false,
             motionTracking: false,
             motionTrackingControl: false,
             showRoadLabels: false,
         })
-        panorama.setPosition(this.randomLatLng)
+        this.panorama.setPosition(this.randomLatLng)
+      },
+      startTimer() {
+        if (!this.hasLocationSelected) {
+          if (this.remainingTime > 0) {
+            setTimeout(() => {
+              this.remainingTime -= 1
+              this.startTimer()
+            }, 1000)
+          } else {
+            // Set a random location if the player didn't select a location in time
+            this.$refs.map.selectRandomLocation(this.getRandomLatLng())
+          }
+        }
       },
       updateScore(distance) {
+        // Update the score and save it into firebase
+        this.hasLocationSelected = true
         this.score += distance
+        
+        if(this.multiplayer){
+          this.room.child('finalScore/player' + this.playerNumber).set(this.score)
+
+          // Wait for other players to guess locations
+          this.dialogTitle = 'Waiting for other players...'
+          this.dialogMessage = true
+        }
+      },
+      showResult() {
+        this.scoreHeader = this.score  // Update the score on header after every players guess locations
+        this.dialogMessage = false
         this.overlay = true
       },
       goToNextRound() {
         // Reset
         this.randomLatLng = null
         this.overlay = false
-        this.isVisibleDialog = false
+        this.hasTimerStarted = false
+        this.hasLocationSelected = false
+        this.isVisibleDialog =false
+
+        if(this.multiplayer){
+          this.dialogMessage = true  // Show the dialog while waiting for other players
+          this.isReady = false  // Turn off the flag so the click event can be added in the next round
+        }
 
         // Update the round
         this.round += 1
 
-        // Replace streetview with new one
-        this.loadStreetView()
-      },
-      playAgain() {
-        // Reset
-        this.randomLatLng = null
-        this.distance = null
-        this.score = 0
-        this.round = 1
-        this.overlay = false
+        if (this.playerNumber == 1 || !this.multiplayer) {
+          this.loadStreetView()
+        } else {
+          // Trigger listener and load the next streetview
+          this.room.child('trigger/player' + this.playerNumber).set(this.round)
+        }
+        this.$refs.map.startNextRound()
 
-        // Load streetview
-        this.loadStreetView()
+      },
+      exitGame() {
+        // Disable the listener and force the players to exit the game
+        this.dialogTitle = 'Redirect to Home Page...'
+        this.dialogText = 'You are forced to exit the game. Redirect to home page after 5 seconds...'
+        this.dialogMessage = true
+        this.room.off()
+        this.room.remove()
+        
+        setTimeout(() => {
+          this.$router.push({ name: 'home' })
+        }, 5000)
+      },
+      finishGame() {
+        if(!this.multiplayer){
+          this.$router.push('/')
+        }else{
+          // Open the dialog while waiting for other players to finsih the game
+          this.dialogTitle = 'Waiting for other players to finish the game...'
+          this.dialogText = ''
+          this.dialogMessage = true
+        }
       }
     },
     mounted() {
-      this.loadStreetView()
-    },
+      this.panorama = new google.maps.StreetViewPanorama(document.getElementById('street-view'))
+      if (this.playerNumber == 1 || !this.multiplayer) {
+        this.loadStreetView()
+      }
+      
+        console.log()
+      if(!this.multiplayer){
+        this.$refs.map.startNextRound()
+      }else{
+        // Set a room name if it's null to detect when the user refresh the page
+        if (this.roomName == null) {
+          this.roomName = 'defaultRoomName'
+        }
+        this.room = firebase.database().ref(this.roomName)
+        this.room.child('active').set(true)
+        this.room.on('value', (snapshot) => {
+          // Check if the room is already removed
+          if (snapshot.hasChild('active')) {
+
+            // Put the player into the current round node if the player is not put yet
+            if (!snapshot.child('round' + this.round).hasChild('player' + this.playerNumber)) {
+
+              this.room.child('round' + this.round).child('player' + this.playerNumber).set(0)
+
+              // Other players load the streetview the first player loaded earlier
+              if (this.playerNumber != 1) {
+                this.randomLat = snapshot.child('streetView/round' + this.round + '/latitude').val()
+                this.randomLng = snapshot.child('streetView/round' + this.round + '/longitude').val()
+                this.isVisibleDialog = snapshot.child('streetView/round' + this.round + '/warning').val()
+
+                this.loadDecidedStreetView()
+              }
+            }
+
+            // Enable guess button when every players are put into the current round's node
+            if (snapshot.child('round' + this.round).numChildren() == snapshot.child('size').val()) {
+
+              // Close the dialog when evryone is ready
+              if (this.isReady == false) {
+                this.dialogMessage = false
+              }
+
+              this.isReady = true
+              this.$refs.map.startNextRound()
+
+              // Countdown timer starts
+              this.timeLimitation = snapshot.child('timeLimitation').val() * 60
+
+              if (this.timeLimitation != 0) {
+                if (!this.hasTimerStarted) {
+                  this.remainingTime = this.timeLimitation
+                  this.startTimer()
+                  this.hasTimerStarted = true
+                }
+              }
+            }
+
+            // Delete the room when everyone finished the game
+            if (snapshot.child('isGameDone').numChildren() == snapshot.child('size').val()) {
+              this.room.child('active').remove()
+            }
+
+          } else {
+            // Force the players to exit the game when 'Active' is removed
+            this.exitGame()
+          }
+        })
+
+        window.addEventListener('popstate', (event) => {
+          // Remove the room when the player pressed the back button on browser
+          this.room.child('active').remove()
+          this.room.off()
+        })
+
+        window.addEventListener('beforeunload', (event) => {
+          // Remove the room when the player refreshes the window
+          this.room.child('active').remove()
+        })
+
+        // Force to exit the game if it's still the name this is set programmatically
+        if (this.roomName == 'defaultRoomName') {
+          this.room.child('active').remove()
+        }
+      }
+    }
   }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
   #game-page {
     position: relative;
     height: 100%; 
@@ -178,6 +382,14 @@
     width: 100%; 
     top: 0; 
     left: 0; 
+    #game-interface--overlay {
+      width: 100%;
+      height: 100%;
+      position: absolute;
+      top: 0;
+      right: 0;
+      display: flex;
+    }
   }
 
   #street-view {
@@ -190,14 +402,11 @@
     z-index: 1;
     margin-top: 56px;
   }
-
   @media (max-width: 450px) {
     #street-view {
       position: fixed;
-      min-height: 100%;
+      min-height: 92%;
       height: 92%;
     }
-
   }
-
 </style>
