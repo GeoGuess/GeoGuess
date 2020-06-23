b219068703f851ce24dea7d9a910c07f51ee4bbd
dim.fort <dimfort85@gmail.com>
2020-06-23 22:53:49 +0200

    Fix Score to Distance

--- src/components/History.vue
+++ src/components/History.vue
@@ -118,7 +118,7 @@ export default {
                     value: 'time',
                 },
                 {
-                    text: "Score",
+                    text: "Distance",
                     value: 'score',
                 },
                 {
