405d1b204857a3f0d8736562572c37626e4e9f12
simonrousseau <simonrousseau22@gmail.com>
2020-10-04 19:13:20 +0200

    add eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    env: {
+        browser: true,
+        es6: true,
+    },
+    extends: ['eslint:recommended', 'plugin:vue/essential'],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+    },
+    plugins: ['vue'],
+    rules: {
+        semi: ['error', 'always'],
+    },
+};
