fb5dfc9735b897ec123a7f6ac1b72a3fa47dbaeb
simonrousseau <simonrousseau22@gmail.com>
2020-10-04 19:21:43 +0200

    add missing languages to vuetify config

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,16 +4,19 @@ import Vuetify from 'vuetify/lib';
 import fr from 'vuetify/es5/locale/fr'
 import en from 'vuetify/es5/locale/en'
 import ja from 'vuetify/es5/locale/ja'
+import cs from 'vuetify/es5/locale/cs'
+import de from 'vuetify/es5/locale/de'
+import es from 'vuetify/es5/locale/es'
 import { checkLanguage } from '../lang';
 
 Vue.use(Vuetify);
 
 export default new Vuetify({  
   lang: {
-    locales: { en, fr, ja },
+    locales: { en, fr, ja, cs, de, es },
     current: localStorage.getItem('language') != null 
     ? localStorage.getItem('language') 
-    : (['en', 'ja', 'fr'].some(checkLanguage) 
+    : (['en', 'ja', 'fr','cs','de','es'].some(checkLanguage) 
       ? navigator.language.split('-')[0] 
       : 'en')
   },
