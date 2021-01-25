ebb21b4ad5c640c4858fd08db037deb754fd97ae
Bilel Jegham <BilelJegham@users.noreply.github.com>
2021-01-25 19:23:05 +0100

    New translations fr.js (Russian)

--- ru/src/lang/locale/ru.js
+++ ru/src/lang/locale/ru.js
@@ -1,101 +1,129 @@
 export default {
     Home: {
-        record: 'ベストスコア',
-        // historyBtn: 'HISTORY',
-        about: 'このサイトについて',
-        // searchBar: {
-        //   customLoaded: 'Custom map has been loaded',
-        //   enterCity: 'Enter city, state or country',
-        // },
-        aboutDescriptions: { 1: 'Geoguess は無料で気軽に遊べる地図を使ったゲームです。        プレーヤーは5ラウンドでランダムに出された場所の正確な場所を推測して競います。        友達と遊ぶ時は最初に部屋を作る人が部屋の名前を決めて部屋を作り、他の人が部屋に入るだけで一緒に遊ぶことができます。' },
-        // customMap: 'CUSTOMS MAP',
-        // customMapDescriptions: {
-        //     1: "You can limit random locations to city, state, or country with the search bar.",
-        //     2: "In the multiplayer, the first player fixes the location.",
-        //     3: "Furthermore, you can make your customs map with ",
-        //     4: "file.",
-        //     5: "Insert the content of the GeoJson map with the button :",
-        //     6: "Customs Map selection : "
-        // },
-        limitation: '制限',
-        limitationDescription: '地図とストリートビューを表示させるのにお金がかかるため制限をかけています。        1日の制限を超えると地図が正しくロードされなくなります。         制限は太平洋標準時での深夜にリセットされます。        このサイトはオープンソースなので制限なく遊びたい方は自分でゲームをセットアップすることもできます。'
+        record: 'Record',
+        historyBtn: 'Historique',
+        about: 'A PROPOS',
+        searchBar: {
+            customLoaded: 'Une carte personnalisée a été chargé',
+            enterCity: 'Saisissez une ville, une région ou un pays'
+        },
+        aboutDescriptions: {
+            1: 'Geoguess est un jeu gratuit sans publicité.                  Les joueurs s\'affrontent en 5 manches pour deviner la plus proche position d\'où ils ont été placé aléatoirement.                 Vous pouvez partager votre score sur les réseaux Sociaux comme Facebook et Twitter.                 Vous pouvez jouer avec vos amis en multijoueurs.                 Le premier joueur créé une salle et décide de sa taille. Une fois créés, les autres joueurs pourront rejoindre la salle à partir de son nom et joueur tous ensemble.',
+            2: 'Ce jeu est issu de '
+        },
+        customMap: 'CARTE PERSONNALISÉE',
+        customMapDescriptions: {
+            1: 'Vous pouvez limiter les positions aléatoires à une ville, région ou pays via la barre de recherche.',
+            2: 'Dans le mode multijoueur, le premier joueurs définit l\'emplacement.',
+            3: 'Par ailleurs, il vous est possible de créer vos propres cartes avec ',
+            4: 'fichier.',
+            5: 'Insérer le contenu du fichier via le bouton :',
+            6: 'Une sélection de maps personnalisés : '
+        },
+        limitation: 'LIMITATION',
+        limitationDescription: 'Actuellement, j\'ai fixé un quota par jour afin que le coût du jeu ne soit pas élevé.            Si la carte ne se charge pas, cela signifie que le quota a été dépassé.            Il est réinitialisé à minuit (heure du pacifique). Désolé pour le dérangement.             Le jeu est open source, ainsi il vous est possible de créer votre propre jeu pour y jouer sans limite.',
+        play: 'Jouer'
     },
     StreetView: {
-        // nearby: {
-        //   title: 'Nearby Position',
-        //   message: 'Unfortunately, we were unable to find a random position in the defined location. However, we have found one nearby 😉',
-        // },
-        waitForOtherPlayers: '他のプレーヤーを待っています...',
-        redirectToHomePage: 'ホーム画面にリダイレクトします...',
-        exitGame: 'ゲームを強制終了しました。5秒後にリダイレクトします...',
-        waitForOtherPlayersToFinish: '他のプレーヤーが終了するのを待っています...'
+        nearby: {
+            title: 'Position Proche',
+            message: 'Malheureusement, nous n\'avons pu trouver une position à l\'emplacement défini. Toutefois, nous en avons trouvé une proche 😉'
+        },
+        waitForOtherPlayers: 'En attente des autres joueurs',
+        redirectToHomePage: 'Redirection vers la page d\'accueil...',
+        exitGame: 'Vous êtes forcé de quitter le jeu. Redirection vers la page d\'accueil dans 5 secondes...',
+        waitForOtherPlayersToFinish: 'En attente des autres joueurs pour finir la partie...'
     },
     Maps: {
-        makeGuess: '推測する',
-        guess: '決定',
-        nextRound: '進む',
-        viewSummary: '結果を見る',
-        exit: '終了する',
-        playAgain: 'もう一度遊ぶ'
+        makeGuess: 'MAKE GUESS',
+        guess: 'DEVINER',
+        nextRound: 'MANCHE SUIVANTE',
+        viewSummary: 'VOIR LE RÉSUMÉ',
+        exit: 'QUITTER',
+        reset: 'RÉINIT.',
+        playAgain: 'REJOUER',
+        infoWindow: {
+            Distance: 'Distance',
+            Points: 'Points'
+        }
     },
     Header: {
-        language: '言語',
-        about: 'このサイトについて',
-        limitation: '制限',
-        contact: 'お問い合わせ'
+        language: 'Langue',
+        about: 'A propos',
+        limitation: 'Limitation',
+        contact: 'Contact'
     },
     HeaderGame: {
-        round: 'ラウンド',
-        score: 'スコア'
+        room: 'SALLE',
+        round: 'MANCHE',
+        distance: 'DISTANCE',
+        kmaway: '{value} km',
+        score: 'SCORE'
     },
-    next: '進む',
-    cancel: 'キャンセル',
-    // DialogCustomMap: {
-    //   title: 'Custom Map',
-    //   invalid: 'Invalid GeoJSON',
-    //   'text': 'Text',
-    //   url: 'Url',
-    //   file: 'File',
-    //   edit: 'Edit Map',
-    //   'fileLabel': 'Select GeoJSON file'
-    // },
-    // History: {
-    //   title: 'History',
-    //   date: 'Date',
-    //   mode: 'Mode',
-    //   time: 'Time',
-    //   distance: 'Distance',
-    //   points: 'Points',
-    //   rank: 'Rank',
-    //   search: 'Search',
-    // },
+    next: 'SUIVANT',
+    cancel: 'ANNULER',
+    DialogCustomMap: {
+        title: 'Carte personnalisée',
+        invalid: 'GeoJSON invalide',
+        text: 'Texte',
+        url: 'Lien',
+        file: 'Fichier',
+        edit: 'Editer la carte',
+        fileLabel: 'Selectionner un fichier GeoJSON'
+    },
+    History: {
+        title: 'Historique',
+        date: 'Date',
+        mode: 'Mode',
+        time: 'Temps',
+        distance: 'Distance',
+        points: 'Points',
+        rank: 'Classement',
+        search: 'Rechercher'
+    },
     DialogRoom: {
-        singlePlayer: '一人で遊ぶ',
-        withFriends: '友達と遊ぶ',
-        invalidRoomName: '無効な名前です。別の名前を入力して下さい。',
-        inProgress: '最初のプレーヤーが部屋を作っています。再度試して下さい。',
-        roomIsFull: 'この部屋は既に満員です。'
+        singlePlayer: 'Un joueur',
+        withFriends: 'Avec des amis',
+        invalidRoomName: 'Nom invalide. Veuillez en choisir un autre.',
+        inProgress: 'Le premier joueur est en train de créer la salle. Veuillez patienter puis recommencer.',
+        roomIsFull: 'La salle est déjà pleine. Essayer une autre.'
     },
-    CardRoomName: { title: '部屋の名前を入力して下さい。' },
-    CardRoomPlayerName: { title: 'プレーヤーの名前を入力して下さい。' },
-    CardRoomSize: { title: '部屋の人数を選択して下さい。' },
+    CardRoomName: { title: 'Saisir le nom de la salle.' },
+    CardRoomPlayerName: {
+        title: 'Saisir votre pseudo.',
+        anonymousPlayerName: 'Anonyme 🕵'
+    },
+    CardRoomSize: { title: 'Saisir la taille de la salle.' },
     CardRoomTime: {
-        title: '1ラウンドの制限時間を選択して下さい。',
-        infinite: '無制限'
+        title: 'Saisir le temps maximum par manche.',
+        infinite: 'Infini'
     },
-    // CardRoomDifficulty: {
-    //   title: 'Set a difficulty level.',
-    //   easy: 'easy (world)',
-    //   medium: 'medium (country)',
-    //   hard: 'hard (city)',
-    // },
-    // DialogSummary: {
-    //   summaryMsgSingle: 'You are <strong>{distance}</strong> km away! Your score is <strong>{points}</strong> points!',
-    //   summaryMsgMulti: '<strong>{playerName}</strong> score is <strong>{points}</strong> ! (<strong>{distance}</strong> kmaway)',
-    //   viewDetails: 'View details',
-    // },
+    CardRoomSettings: { title: 'Paramétre de la partie' },
+    CardRoomDifficulty: {
+        title: 'Saisir la difficulté.',
+        easy: 'Facile (monde)',
+        medium: 'Moyen (pays)',
+        hard: 'Difficile (ville)'
+    },
+    DialogSummary: {
+        summaryMsgSinglePoints: 'Votre score est de <strong>{points}</strong> points!',
+        summaryMsgSingleDistance: 'Vous êtes à <strong>{distance}</strong> km !',
+        summaryMsgMultiPoints: '<strong>{playerName}</strong> marque <strong>{points}</strong> points ! ',
+        summaryMsgMultiDistance: '(<strong>{distance}</strong> km)',
+        viewDetails: 'Voir les détails'
+    },
     Footer: {
-        // under: 'under',
-        privacyPolicy: 'プライバシーポリシー'
+        under: 'sous',
+        privacyPolicy: 'Politique de confidentialité'
+    },
+    urlCopied: 'Url copiée',
+    DetailsMap: { moreInfo: 'En savoir plus' },
+    modes: {
+        classic: 'Classique',
+        country: 'Pays'
+    },
+    Demo: {
+        message: 'Tu souhaites contribuer au projet et le rendre meilleur, ton aide est la bienvenue :  Traduction 🔠, Créateur de carte 🌍, Code 💻, Idée 💡, Beta test 👀.',
+        btn: 'Rejoins-nous'
     }
 };
