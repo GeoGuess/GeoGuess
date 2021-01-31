425733fbbb08965b6d3b00bf3d3de601c0c5939c
BilelJegham <bilel@jegham.com>
2021-01-31 22:20:56 +0100

    feat: gmap a language

--- src/main.js
+++ src/main.js
@@ -21,6 +21,7 @@ Vue.use(VueClipboard);
 Vue.use(GmapVue, {
     load: {
         key: process.env.VUE_APP_API_KEY,
+        language: localStorage.getItem('language'),
     },
 });
 Vue.config.productionTip = false;
