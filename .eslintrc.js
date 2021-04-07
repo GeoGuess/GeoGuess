79f0cbeed2d30ff2e683a8c53efc84905b3de025
BilelJegham <contact.bileljegham@gmail.com>
2021-04-07 22:43:42 +0200

    fix: indent

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
     extends: [
         'eslint:recommended',
         'plugin:vue/essential',
-		'plugin:vue/recommended',
+        'plugin:vue/recommended',
     ],
     globals: {
         launchQueue: 'readonly',
