62e1e979b4ce7eab599201c6aa35ca25d0582fba
BilelJegham <contact.bileljegham@gmail.com>
2021-04-25 21:58:17 +0200

    hello

--- src/discord.js
+++ src/discord.js
@@ -3,34 +3,43 @@
 const RPC = require('discord-rpc');
 
 const startTimestamp = new Date();
-function makeClient(clientId = '') {
+function makeClient(clientId = '835891285940240406') {
     const client = new RPC.Client({ transport: 'websocket' });
 
     RPC.register(clientId);
 
     client.on('ready', () => {
+        // eslint-disable-next-line no-debugger
         debugger;
+        // eslint-disable-next-line no-console
     console.log('Logged in as', client.application.name);
+    // eslint-disable-next-line no-console
     console.log('Authed for user', client.user.username);
+      setInterval(()=>{
+        client.setActivity({
+          details: `booped times`,
+  state: 'in slither party',
+  startTimestamp,
+  largeImageKey: 'snek_large',
+  largeImageText: 'tea is delicious',
+  smallImageKey: 'snek_small',
+  smallImageText: 'i am my own pillows',
+  instance: false,
+  });
+      },1);
 
-    client.setActivity({
-            details: `booped times`,
-    state: 'in slither party',
-    startTimestamp,
-    largeImageKey: 'snek_large',
-    largeImageText: 'tea is delicious',
-    smallImageKey: 'snek_small',
-    smallImageText: 'i am my own pillows',
-    instance: false,
+
     });
-
-    });
-
+  
 
     // Log in to RPC with client id
-    client.login({clientId }).catch(console.error);
-    
-    console.log('Logged in as', client);
+        // eslint-disable-next-line no-console
+    client.login({clientId, scopes: ['rpc', 'rpc.api', 'messages.read'] }).catch(console.error);
+  
+        // eslint-disable-next-line no-console
+    console.log( client);
+    // eslint-disable-next-line no-console
+console.log( clientId);
 
   return client;
 }
