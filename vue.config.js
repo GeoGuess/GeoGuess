fca3a3f5b023628914c13ed1775f561f4b6adcde
Bilel JEGHAM <contact.bileljegham@gmail.com>
2021-11-01 12:47:01 +0100

    fix: service worker _redirect

--- vue.config.js
+++ vue.config.js
@@ -5,4 +5,7 @@ module.exports = {
             skipWaiting: true,
         },
     },
+    publicPath: process.env.NODE_ENV === 'production' && process.env.VUE_APP_PUBLIC_PATH
+        ? process.env.VUE_APP_PUBLIC_PATH
+        : '/'
 };
