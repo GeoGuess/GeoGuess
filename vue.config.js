3b0da7cf7fdd4ebafc34075e6bcccd90e275825d
Bilel JEGHAM <contact.bileljegham@gmail.com>
2021-11-01 13:48:11 +0100

    fix: config pwa

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,5 @@
 module.exports = {
     transpileDependencies: ['vuetify'],
-    pwa: {
-        workboxOptions: {
-            skipWaiting: true,
-        },
-    },
     publicPath: process.env.NODE_ENV === 'production' && process.env.VUE_APP_PUBLIC_PATH
         ? process.env.VUE_APP_PUBLIC_PATH
         : '/'
