b1d7ae54e3ca1260deca6681fa8d6ce7d7f66b87
ElectricS01 <103579308+ElectricS01@users.noreply.github.com>
2023-10-04 18:16:52 +1100

    Update /store/index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,52 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import alertModule from './modules/alert.store';
-import areaModule from './modules/area.store';
-import homeModule from './modules/home.store';
-import settingsModule from './modules/settings.store';
 
 Vue.use(Vuex);
 
+function loadModules() {
+    const context = import.meta.globEager('./modules/*.js');
+
+    const modules = Object.keys(context)
+        .map((key) => ({ key, name: key.match(/([a-z_]+)(.store)?\.js$/i)[1] }))
+        .reduce(
+            (modules, { key, name }) => ({
+                ...modules,
+                [`${name}Store`]: context[key].default,
+            }),
+            {}
+        );
+
+    return { context, modules };
+}
+
+const { context, modules } = loadModules();
 const store = new Vuex.Store({
-    modules: {
-        alertStore: alertModule,
-        area: areaModule,
-        homeStore: homeModule,
-        settingsStore: settingsModule,
-    },
+    modules,
 });
 
+if (import.meta.hot) {
+    import.meta.hot.accept(
+        [
+            './modules/alert.store.js',
+            './modules/area.store.js',
+            './modules/home.store.js',
+            './modules/settings.store.js',
+        ],
+        (newModules) => {
+            if (newModules) {
+                const { modules } = loadModules();
+                Object.keys(context).map((key, i) => {
+                    if (newModules[i]) {
+                        const name = key.match(/([a-z_]+)(.store)?\.js$/i)[1];
+                        modules[name + 'Store'] = newModules[i].default;
+                    }
+                });
+
+                store.hotUpdate({
+                    modules,
+                });
+            }
+        }
+    );
+}
 export default store;
